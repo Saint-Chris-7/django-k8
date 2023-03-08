6126c4a4569dcdac3c5b01f72a3a7e7007f90ef0
Saint-Chris-7 <chrisgathuthi@gmail.com>
2023-03-08 07:31:11 +0300

    docker and kubernetes

--- web/djang_k8/settings.py
+++ web/djang_k8/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +21,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-v6sgj)9=%0jrfs9076zczfwkieh_))=qduj1n-m4nyf+$ll=8i'
-
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = str(os.environ.get("DEBUG")) == "1"
 
 ALLOWED_HOSTS = []
 
@@ -73,12 +73,32 @@ WSGI_APPLICATION = 'djang_k8.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+import os
+DB_USERNAME = os.environ.get("POSTGRES_USER")
+DB_PASSWORD = os.environ.get("POSTGRES_PASSWORD")
+DB_DATABASE = os.environ.get("POSTGRES_DB")
+DB_HOST = os.environ.get("POSTGRES_HOST")
+DB_PORT = os.environ.get("POSTGRES_PORT")
+DB_IS_AVAIL = all([DB_DATABASE,DB_HOST,DB_PASSWORD,DB_PASSWORD,DB_USERNAME])
+
+
+if DB_IS_AVAIL:
+    DATABASES = {
+        "default":{
+            "ENGINE":"django.db.backends.postgresql",
+            "NAME":DB_DATABASE,
+            "USER":DB_USERNAME,
+            "PASSWORD":DB_PASSWORD,
+            "HOST":DB_PORT,
+            "PORT":DB_HOST
+        }
     }
-}
 
 
 # Password validation
